2169ca562808496960da7659753ed0473c467355
naveen kumar <kaviidishu@gmail.com>
2025-06-15 12:34:30 +0530

    Final update: Fixed animations and styling

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,16 @@
 import Footer from "../components/Footer";
+import react from "../assets/images/react.svg";
+import Html from "../assets/images/html.svg";
+import Css from "../assets/images/css.svg";
+import Js from "../assets/images/js.svg";
+import Ts from "../assets/images/ts.png";
+import react_native from "../assets/images/react-native.svg";
+import Api from "../assets/images/api.png";
+import Tailwind from "../assets/images/tailwind.svg";
+import Shadcn from "../assets/images/shadcn.png";
+import Node_js from "../assets/images/node-js.png";
+import Github from "../assets/images/github.png";
+import Wordpress from "../assets/images/wordpress.png";
 
 export default function Projects() {
   return (
@@ -11,25 +23,61 @@ export default function Projects() {
               <div class="skills-list">
                 <h5>Languages</h5>
                 <ul>
-                  <li>HTML</li>
-                  <li>CSS</li>
-                  <li>JavaScript</li>
-                  <li>TypeScript</li>
+                  <li>
+                    <img src={Html} alt="react image" />
+                    HTML
+                  </li>
+                  <li>
+                    <img src={Css} alt="react image" />
+                    CSS
+                  </li>
+                  <li>
+                    <img src={Js} alt="react image" />
+                    JavaScript
+                  </li>
+                  <li>
+                    <img src={Ts} alt="react image" />
+                    TypeScript
+                  </li>
                 </ul>
                 <h5>Libraries</h5>
                 <ul>
-                  <li>React</li>
-                  <li>React Native (experience)</li>
-                  <li>Context API</li>
+                  <li>
+                    <img src={react} alt="react image" />
+                    React
+                  </li>
+                  <li>
+                    <img src={react_native} alt="react image" />
+                    React Native (experience)
+                  </li>
+                  <li>
+                    <img src={Api} alt="react image" />
+                    Context API
+                  </li>
                 </ul>
                 <h5>Frameworks & Tools</h5>
                 <ul>
-                  <li>Tailwind CSS</li>
-                  <li>shadcn/ui</li>
-                  <li>Node.js</li>
-                  <li>Express.js</li>
-                  <li>GitHub</li>
-                  <li>WordPress</li>
+                  <li>
+                    <img src={Tailwind} alt="react image" />
+                    Tailwind CSS
+                  </li>
+                  <li>
+                    <img src={Shadcn} alt="react image" />
+                    shadcn/ui
+                  </li>
+                  <li>
+                    <img src={Node_js} alt="react image" />
+                    Node.js
+                  </li>
+
+                  <li>
+                    <img src={Github} alt="react image" />
+                    GitHub
+                  </li>
+                  <li>
+                    <img src={Wordpress} alt="react image" />
+                    WordPress
+                  </li>
                 </ul>
               </div>
             </div>
